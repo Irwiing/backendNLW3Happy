e7cf46f6ece753d5641bddad60467c69f3ac30a3
Irwiing <irwingtguarnieri@gmail.com>
2020-10-18 23:05:25 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler)
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(process.env.PORT || 3333);
